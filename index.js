b9bbd00306e9e0005d77ceebeadd286465173d2c
Yongcheng Ma <yma@rocketsoftware.com>
2019-09-03 14:15:30 +0800

    add basic auth

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
-const argv=require("yargs").alias('p',"port").describe("p","specify the listen port").argv;
+const argv=require("yargs").alias('p',"port").describe("p","specify the listen port").alias("a","auth").describe("a","specify basic auth in user:pass format").argv;
 const express=require("express")
 const cp=require('child_pty')
 const servestatic=require("serve-static")
 var port=process.env.PORT ||3000;
 if(argv.port) port=argv.port
 var app=express()
+if(argv.auth)
+{
+	var users={};
+	if(Array.isArray(argv.auth))
+	{
+		for(var i=0;i<argv.auth.length;i++)
+		{
+			var chunks=argv.auth[i].split(":");
+			users[chunks.shift()]=chunks.join(":");
+		}
+	}
+	else
+	{
+		var chunks=argv.auth.split(":")
+		users[chunks.shift()]=chunks.join(":");
+	}
+	const bauth=require("express-basic-auth");
+	app.use(bauth({
+		users:users,
+		challenge:true,
+		realm:"webterminal"
+		}));
+}
 var http=require("http").createServer(app);
 var io=require("socket.io")(http);
 var p=cp.spawn("bash",[],{name:"xterm-color",columns:80,rows:25})
